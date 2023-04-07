df31cbb576a5c2a5d10c6f0119ae27c62d5a4b06
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-04-06 23:39:14 -0400

    Install Flask-Cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_cors import CORS
 from housing_price import HousingPrice
 
 app = Flask(__name__)
 api = Api(app)
+cors = CORS(app)
 
 api.add_resource(HousingPrice, "/predict")
 
